e007aed08d50bf873556f6e44a68c3c69dcfce33
YESTERDAYONCEMO\user <rlagnscjf123@naver.com>
2026-03-19 16:51:21 +0900

    GitStudyWindow
    New UProject Created.

---
+++ Source/GitStudyWindow/GitStudyWindow.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GitStudyWindow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitStudyWindow, "GitStudyWindow" );
